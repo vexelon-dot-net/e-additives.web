e2f28c576f11520d8755003fed0b0e98c62c4240
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-10-17 21:40:14 +0200

    Fix: Quick search now works on search page results

--- js/app.js
+++ js/app.js
@@ -256,7 +256,9 @@ require(['sammy', 'config', 'api', 'bindings', 'breadcrumbs', 'mustache', 'i18n!
                     }
                     context.data = formatAdditivesData(data);
                     context.locale = Locale;
-                    context.partial('partials/search-additives.ms');                        
+                    context.partial('partials/search-additives.ms', function() {
+                        $('table').footable();
+                    });                        
                 });                
             });
         }); 
