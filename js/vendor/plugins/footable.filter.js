10f2f566d888359762e835d64523a1e9d8448ce8
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-10-17 21:38:30 +0200

    Fixed issue #20. Only name and code included in search.
    This fix was done directly in the footable.filter.js plugin.
    This must be noted when/if the plugin is being update.

--- js/vendor/plugins/footable.filter.js
+++ js/vendor/plugins/footable.filter.js
@@ -13,7 +13,7 @@
                 var $t = $(this),
                     $table = $t.parents('table:first'),
                     filter = $table.data('current-filter').toUpperCase(),
-                    text = $t.find('td').text();
+                    text = $t.find('td[data-filter]').text(); // --> added by p.petrov (issue #20)
                 if (!$table.data('filter-text-only')) {
                     $t.find('td[data-value]').each(function () {
                         text += $(this).data('value');
